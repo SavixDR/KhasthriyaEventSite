2834826234c2ecc800875bca1eca7d4d0b046f4b
Savindu Gunasekara <savindugunasekara@gmail.com>
2024-06-23 07:24:54 +0530

    artists added

--- app/(root)/(home)/events/[id]/page.tsx
+++ app/(root)/(home)/events/[id]/page.tsx
@@ -4,7 +4,9 @@ import './Event.css';
 import { useParams } from 'next/navigation';
 import { events } from '../../../../../events'; 
 import { motion } from 'framer-motion';
-
+import { artists } from '../../../../../constants/index';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAward, faCalendar, faLocationDot, faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
 
 const Booking = () => {
   const { id } = useParams();
@@ -24,12 +26,13 @@ const Booking = () => {
         />
       </div>
 
-      <div className="details flex justify-center bg-[#1a1a1a] flex-col items-center w-full">
+      <div className="details flex justify-center bg-black flex-col items-center w-full">
         <motion.div
-        initial={{opacity:0,y:100}}
-        animate={{opacity:1,y:-300}}
-        transition={{duration:2}}
-        className="tile w-[90%] shadow-xl bg-[#0e0e0e] rounded-lg flex ">
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: -300 }}
+          transition={{ duration: 2 }}
+          className="tile w-[90%] shadow-xl bg-[#0e0e0e] rounded-lg flex "
+        >
           <div className="sm:hidden md:block md:w-1/2 lg:w-1/3 p-8 rounded-lg">
             <img
               src={event.poster}
@@ -41,31 +44,46 @@ const Booking = () => {
             <h1 className="sm:text-center md:text-left text-3xl">
               {event.title}
             </h1>
-            <p className="mt-5 max-w-[700px]">
+            <p className="mt-10 max-w-[700px]">
               HASKY ENTERTAINMENT Proudly Presents "SARAMA LIVE IN CONCERT" on
               the 11th of April 2024 at Sahas Uyana - Kandy FT : Sanuka & Windy
               | Vini Products | Chandika Lilan | Milan | Music by Memory SL
               Band.
             </p>
-            <div className="mt-5">
+            <div className="mt-20">
               <div className="flex items-center gap-3 mb-3">
-                <i className="fa-solid text-xl text-[#FFD700] fa-calendar"></i>
+              <FontAwesomeIcon icon={faCalendar} />
                 <p>{event.date}</p>
               </div>
               <div className="flex items-center gap-x-3 mb-3">
-                <i className="fa-solid text-xl text-[#FFD700] fa-location-dot"></i>
+              <FontAwesomeIcon icon={faLocationDot}  />
                 <p>Sahas Uyana - Kandy</p>
               </div>
               <div className="flex items-center gap-x-3 mb-3">
-                <i className="fa-solid text-xl text-[#FFD700] fa-award"></i>
+              <FontAwesomeIcon icon={faAward}  />
                 <p>Organized by HASKY ENTERTAINMENT</p>
               </div>
             </div>
-
           </div>
         </motion.div>
 
-        <div className='w-full -mt-[200px] h-[1000px]'>hi</div>
+        <div className="w-full -mt-[200px] h-[1000px]">
+          <div className="mx-20">
+            <h1 className="text-white text-4xl font-bold text-center md:text-left md:mx-10">Featured Artists</h1>
+            <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6">
+              {artists.map((artist, index) => (
+                <div key={index} className="my-10 flex items-center flex-col">
+                  <img 
+                    src={artist.imgUrl} 
+                    alt={artist.name} 
+                    className="w-32 h-32 rounded-full object-cover mb-4" 
+                  />
+                  <p className="text-white text-center text-lg">{artist.name}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- constants/index.ts
+++ constants/index.ts
@@ -19,3 +19,19 @@ export const facts =[
         imageUrl:"https://assets.website-files.com/646f65e37fe0275cfb808405/646f66cdeeb4ddfdae25a274_CodepenLogo.svg"
     },
 ]
+
+export const artists =[
+   { name:"Rookantha Gunathilaka",
+    imgUrl:"https://alchetron.com/cdn/rookantha-gunathilake-ebca8e19-8227-4227-aeb2-7ae94d3b9e3-resize-750.jpg"
+   },
+   { name:"Kasun Kalhara",
+    imgUrl:"https://upload.wikimedia.org/wikipedia/commons/4/4b/Kasun_Kalhara.jpg"
+   },
+   { name:"Hirushi Jayasena",
+    imgUrl:"https://i1.sndcdn.com/artworks-SscyiQAy6KEvvOpk-zRCyyQ-t500x500.jpg"
+   },
+   { name:"Kanchana Anuradhi",
+    imgUrl:"https://i.scdn.co/image/ab6761610000e5eb7d5aa798103bfb8562427274"
+   },
+  
+]
